5e9de8389d94af9da247f62c7b981732947a18eb
Pranav Dodda <pranavdodda3@gmail.com>
2025-07-22 21:12:27 -0500

    jsx files

--- portfolio/vite.config.js
+++ portfolio/vite.config.js
@@ -6,9 +6,10 @@ import path from "path"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  resolve: {
+  resolve:{
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  
   },
 });
